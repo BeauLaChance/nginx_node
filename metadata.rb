1fe65ab243cf406532fba4b3b9b193f1afbd245d
BeauLaChance <beau.lachance@gmail.com>
2016-04-07 11:22:32 -0400

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name 'nginx_node'
+maintainer 'Beau LaChance'
+maintainer_email 'beau.lachance@gmail.com'
+license 'all_rights'
+description 'Installs/Configures nginx_node'
+long_description 'Installs/Configures nginx_node'
+version '0.1.0'
+
+issues_url 'http://www.example.com/issues'
+source_url 'http://www.example.com/source'
